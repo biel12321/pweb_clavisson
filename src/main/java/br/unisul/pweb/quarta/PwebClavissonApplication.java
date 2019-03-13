a05797e378e0fdf1b9d40ff245edb3daaa7d5ca6
biel12321 <gabriellw.grb@gmail.com>
2019-03-13 20:11:54 -0300

    Projeto Criado

---
+++ src/main/java/br/unisul/pweb/quarta/PwebClavissonApplication.java
@@ -0,0 +1,13 @@
+package br.unisul.pweb.quarta;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PwebClavissonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PwebClavissonApplication.class, args);
+	}
+
+}
