f7235e462520462f5a48849df28243c3ec2dd313
biel12321 <gabriellw.grb@gmail.com>
2019-03-27 19:36:33 -0300

    Adicionando Estado

---
+++ src/main/java/br/unisul/pweb/quarta/repositories/EstadoRepository.java
@@ -0,0 +1,9 @@
+package br.unisul.pweb.quarta.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import br.unisul.pweb.quarta.domain.Estado;
+
+public interface EstadoRepository extends JpaRepository<Estado, Integer>{
+
+}
--- src/main/java/br/unisul/pweb/quarta/services/DbService.java
+++ src/main/java/br/unisul/pweb/quarta/services/DbService.java
@@ -7,24 +7,28 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 import br.unisul.pweb.quarta.domain.Categoria;
+import br.unisul.pweb.quarta.domain.Estado;
 import br.unisul.pweb.quarta.repositories.CategoriaRepository;
+import br.unisul.pweb.quarta.repositories.EstadoRepository;
 
 @Service
 public class DbService {
 
 	@Autowired
 	private CategoriaRepository categoriaRepository;
+	@Autowired
+	private EstadoRepository estadoRepository;
 	
 	public void inicializaBancoDeDados() throws ParseException {
 		
 		Categoria cat1 = new Categoria(null, "Informática");
 		Categoria cat2 = new Categoria(null, "Escritório");
-		Categoria cat3 = new Categoria(null, "Cama mesa e banho");
-		Categoria cat4 = new Categoria(null, "Eletrônicos");
-		Categoria cat5 = new Categoria(null, "Jardinagem");
-		Categoria cat6 = new Categoria(null, "Decoração");
-		Categoria cat7 = new Categoria(null, "Perfumaria");
-		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3, cat4, cat5, cat6, cat7));
+		Categoria cat3 = new Categoria(null, "Decoração");
+		Estado est1 = new Estado(null, "Florianopolis");
+		Estado est2 = new Estado(null, "Blumenal");
+		Estado est3 = new Estado(null, "Jardin dourado");
+		categoriaRepository.saveAll(Arrays.asList(cat1, cat2, cat3));
+		estadoRepository.saveAll(Arrays.asList( est1, est2, est3));
 		
 	}
 }
